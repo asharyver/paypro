1c42f9a7120e1e9e53d2af5f394accaa258fd037
Ashary Vermaysha <asharyver13@gmail.com>
2018-02-24 19:27:50 +0700

    Fix Tombol Language & Download Di Header

--- application/views/template/topbar.php
+++ application/views/template/topbar.php
@@ -4,6 +4,18 @@
 	<li><a href="<?=site_url('Promos'); ?>" class="effect-3">Promo</a></li>
 	<li><a href="<?=site_url('Merchants'); ?>" class="effect-3">Merchant</a></li>
 	<li><a href="<?=site_url('Contacts'); ?>" class="effect-3">Contact Us</a></li>
-	<li><a href="" ><img src="<?=base_url('web/images/eng.png'); ?>"></a></li>
-	<li class="btn-download"><a class="hrf-std" href="#">Download Paypro</a></li>
+	<li class="language-btn dropdown">
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="<?=base_url('web/images/id.png');?>" /> <span class="caret"></span></a>
+		<ul class="dropdown-menu">
+			<li class="dropdown-item"><a href=""><img src="<?=base_url('web/images/id.png');?>" /> Indonesian</a></li>
+			<li class="dropdown-item"><a href=""><img src="<?=base_url('web/images/eng.png');?>" /> English</a></li>
+		</ul>
+	</li>
+	<li class="download-paypro dropdown">
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown">Download PayPro</a>
+		<ul class="dropdown-menu">
+			<li class="dropdown-item"><a href="">Play Store</a></li>
+			<li class="dropdown-item"><a href="">App Store</a></li>
+		</ul>
+	</li>
 </ul>
